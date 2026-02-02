b717a7a4c29a737dc094f416277bd40a41dda78c
Heidi Mork <heidimork@yahoo.no>
2026-02-02 22:04:37 +0100

    update port and extensions for dev container

--- Program.fs
+++ Program.fs
@@ -25,6 +25,7 @@ let main _ =
                 webHostBuilder
                     .Configure(configureApp)
                     .ConfigureServices(configureServices)
+                    .UseUrls("http://*:5000")
                     |> ignore)
         .Build()
         .Run()
