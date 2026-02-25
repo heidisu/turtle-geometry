6255c2de3855c58347b0af49a20c1f5743ded2aa
Heidi Mork <heidimork@yahoo.no>
2026-02-25 21:47:45 +0100

    add branch

--- Core.fs
+++ Core.fs
@@ -17,5 +17,16 @@ let square = [
     Right 90
 ]
 
+let rec branch length level =
+    if level = 0 then []
+    else
+        [ Forward length; Left 45]
+        @ branch (length / 2) (level - 1)
+        @ [ Right 90]
+        @ branch (length / 2) (level - 1)
+        @ [Left 45; Back length]
+
+
+
 // the turtle commands used to create svg on web page
-let webPagePath = square
+let webPagePath = branch 100 3
