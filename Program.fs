92e9bf1a91ea491878222f39eb0ddccb01ac5290
Heidi Mork <heidimork@yahoo.no>
2026-08-13 22:35:40 +0200

    add arrow to indicate where the turtle is pointing

--- Program.fs
+++ Program.fs
@@ -7,12 +7,22 @@ open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Giraffe.ViewEngine
 open TurtleGeometry.Core
+open Microsoft.AspNetCore.Http
+open System
 
-let webApp =
-    choose [
-        route "/"
-        >=> htmlString (RenderView.AsString.htmlDocument (WebPage.htmlPage webPagePath))
-    ]
+let turtleHandler: HttpHandler =
+    fun (next: HttpFunc) (ctx: HttpContext) ->
+        let showArrow =
+            match ctx.TryGetQueryStringValue "arrow" with
+            | None -> false
+            | Some arrow ->
+                match Boolean.TryParse arrow with
+                | true, value -> value
+                | false, _ -> false
+
+        htmlString (RenderView.AsString.htmlDocument (WebPage.htmlPage webPagePath showArrow)) next ctx
+
+let webApp = choose [ route "/" >=> turtleHandler ]
 
 let configureApp (app: IApplicationBuilder) = app.UseGiraffe webApp
 
--- WebPage.fs
+++ WebPage.fs
@@ -12,13 +12,27 @@ type ViewBox = { XRange: Range; YRange: Range }
 type Point = { X: float; Y: float }
 type Direction = { Dx: float; Dy: float }
 
-let svg width height path viewBox =
+let svg width height pos dir path viewBox =
     let strokeWidth = 1.0
+    let dirLength = sqrt (dir.Dx * dir.Dx + dir.Dy * dir.Dy)
+
+    let normalized = {
+        Dx = dir.Dx / dirLength
+        Dy = dir.Dy / dirLength
+    }
+
+    let turtleWidth = viewBox.XRange.Max - viewBox.XRange.Min
+    let turtleHeight = viewBox.YRange.Max - viewBox.YRange.Min
+    let arrowSize = turtleHeight * 20.0 / (float) height
+
+    let arrow =
+        $"""<path d="M {pos.X} {pos.Y} l {arrowSize * normalized.Dx} {arrowSize * normalized.Dy}" stroke="red" stroke-width="%.1f{strokeWidth}"  vector-effect="non-scaling-stroke" />"""
+
 
     $"""
-    <svg xmlns="http://www.w3.org/2000/svg" style="background-color:white" width="%i{width}" height="%i{height}" viewBox="%.4f{viewBox.XRange.Min - strokeWidth},%.4f{viewBox.YRange.Min - strokeWidth},%.4f{viewBox.XRange.Max - viewBox.XRange.Min + 2.0 * strokeWidth},%.4f{viewBox.YRange.Max - viewBox.YRange.Min + 2.0 * strokeWidth}">
-        <path stroke="%s{color}" stroke-width="%.1f{strokeWidth}" fill="white" vector-effect="non-scaling-stroke" d="%s{path}">
-        </path>
+    <svg xmlns="http://www.w3.org/2000/svg" overflow="visible" box-shadow="0px -0px 100px transparent" style="background-color:white" width="%i{width}" height="%i{height}" viewBox="%.4f{viewBox.XRange.Min - strokeWidth},%.4f{viewBox.YRange.Min - strokeWidth},%.4f{turtleWidth + 2.0 * strokeWidth},%.4f{turtleHeight + 2.0 * strokeWidth}">
+        <path stroke="%s{color}" stroke-width="%.1f{strokeWidth}" fill="white" vector-effect="non-scaling-stroke" d="%s{path}"/>
+        {arrow}
     </svg>
 """
 
@@ -33,9 +47,9 @@ let updateViewBox point viewBox = {
     }
 }
 
-let rec calculatePath (point: Point) (dir: Direction) (pathBuilder: StringBuilder) (viewBox: ViewBox) turtlePath =
+let rec calculatePath (pos: Point) (dir: Direction) (pathBuilder: StringBuilder) (viewBox: ViewBox) turtlePath =
     match turtlePath with
-    | [] -> pathBuilder, viewBox
+    | [] -> pos, dir, pathBuilder, viewBox
     | command :: xs ->
         match command with
         | Forward a ->
@@ -43,8 +57,8 @@ let rec calculatePath (point: Point) (dir: Direction) (pathBuilder: StringBuilde
             let factor = sqrt (afloat * afloat / (dir.Dx * dir.Dx + dir.Dy * dir.Dy))
 
             let newPoint = {
-                X = point.X + factor * dir.Dx
-                Y = point.Y + factor * dir.Dy
+                X = pos.X + factor * dir.Dx
+                Y = pos.Y + factor * dir.Dy
             }
 
             pathBuilder.Append($" L%.4f{newPoint.X},%.4f{newPoint.Y}") |> ignore
@@ -53,19 +67,19 @@ let rec calculatePath (point: Point) (dir: Direction) (pathBuilder: StringBuilde
             let phi = atan2 dir.Dy dir.Dx
             let apol = float a / 360.0 * 2.0 * Math.PI
             let newPhi = phi + apol
-            calculatePath point { Dx = cos newPhi; Dy = sin newPhi } pathBuilder viewBox xs
+            calculatePath pos { Dx = cos newPhi; Dy = sin newPhi } pathBuilder viewBox xs
         | Left a ->
             let phi = atan2 dir.Dy dir.Dx
             let apol = float a / 360.0 * 2.0 * Math.PI
             let newPhi = phi - apol
-            calculatePath point { Dx = cos newPhi; Dy = sin newPhi } pathBuilder viewBox xs
+            calculatePath pos { Dx = cos newPhi; Dy = sin newPhi } pathBuilder viewBox xs
         | Back a ->
             let afloat = float a
             let factor = sqrt (afloat * afloat / (dir.Dx * dir.Dx + dir.Dy * dir.Dy))
 
             let newPoint = {
-                X = point.X - factor * dir.Dx
-                Y = point.Y - factor * dir.Dy
+                X = pos.X - factor * dir.Dx
+                Y = pos.Y - factor * dir.Dy
             }
 
             pathBuilder.Append($" M%.4f{newPoint.X},%.4f{newPoint.Y}") |> ignore
@@ -74,7 +88,7 @@ let rec calculatePath (point: Point) (dir: Direction) (pathBuilder: StringBuilde
 let turtleToSvgPath turtlePath =
     let stringBuilder = new StringBuilder()
 
-    let pathBuilder, viewBox =
+    let pos, dir, pathBuilder, viewBox =
         calculatePath
             { X = 0.0; Y = 0.0 }
             { Dx = 0.0; Dy = -1.0 }
@@ -85,18 +99,18 @@ let turtleToSvgPath turtlePath =
             }
             turtlePath
 
-    pathBuilder.ToString(), viewBox
+    pos, dir, pathBuilder.ToString(), viewBox
 
 
-let htmlPage turtlePath =
-    let path, viewBox = turtleToSvgPath turtlePath
+let htmlPage turtlePath showArrow =
+    let pos, dir, path, viewBox = turtleToSvgPath turtlePath
 
     html [] [
         head [] [ title [] [ str "Turtle Geometry" ] ]
         body [ attr "style" $"color: {color}" ] [
             div [ attr "align" "center" ] [
                 h1 [] [ str "Turtle Geometry" ]
-                div [] [ rawText (svg 450 450 path viewBox) ]
+                div [] [ rawText (svg 450 450 pos dir path viewBox) ]
                 div [] [
                     div [] [ str $"x range: [%.2f{viewBox.XRange.Min}, %.2f{viewBox.XRange.Max}]" ]
                     div [] [ str $"y range: [%.2f{viewBox.YRange.Min}, %.2f{viewBox.YRange.Max}]" ]
