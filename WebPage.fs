c3456655227e20fa12fad6efd3019e9453b11149
Heidi Mork <heidi@morkcloud.no>
2026-08-13 20:56:55 +0200

    Improve webpage model (#36)

--- WebPage.fs
+++ WebPage.fs
@@ -7,83 +7,99 @@ open System.Text
 
 let color = "#5d009b"
 
-let svg width height path (xMin, xMax, yMin, yMax) =
+type Range = { Min: float; Max: float }
+type ViewBox = { XRange: Range; YRange: Range }
+type Point = { X: float; Y: float }
+type Direction = { Dx: float; Dy: float }
+
+let svg width height path viewBox =
     let strokeWidth = 1.0
 
     $"""
-    <svg xmlns="http://www.w3.org/2000/svg" style="background-color:white" width="%i{width}" height="%i{height}" viewBox="%.4f{xMin - strokeWidth},%.4f{yMin - strokeWidth},%.4f{xMax - xMin + 2.0 * strokeWidth},%.4f{yMax - yMin + 2.0 * strokeWidth}">
+    <svg xmlns="http://www.w3.org/2000/svg" style="background-color:white" width="%i{width}" height="%i{height}" viewBox="%.4f{viewBox.XRange.Min - strokeWidth},%.4f{viewBox.YRange.Min - strokeWidth},%.4f{viewBox.XRange.Max - viewBox.XRange.Min + 2.0 * strokeWidth},%.4f{viewBox.YRange.Max - viewBox.YRange.Min + 2.0 * strokeWidth}">
         <path stroke="%s{color}" stroke-width="%.1f{strokeWidth}" fill="white" vector-effect="non-scaling-stroke" d="%s{path}">
         </path>
     </svg>
 """
 
-let rec calculatePath (x, y) (dx, dy) (pathBuilder: StringBuilder) xMin xMax yMin yMax turtlePath =
+let updateViewBox point viewBox = {
+    XRange = {
+        Min = min viewBox.XRange.Min point.X
+        Max = max viewBox.XRange.Max point.X
+    }
+    YRange = {
+        Min = min viewBox.YRange.Min point.Y
+        Max = max viewBox.YRange.Max point.Y
+    }
+}
+
+let rec calculatePath (point: Point) (dir: Direction) (pathBuilder: StringBuilder) (viewBox: ViewBox) turtlePath =
     match turtlePath with
-    | [] -> pathBuilder, (xMin, xMax, yMin, yMax)
+    | [] -> pathBuilder, viewBox
     | command :: xs ->
         match command with
         | Forward a ->
             let afloat = float a
-            let factor = sqrt (afloat * afloat / (dx * dx + dy * dy))
-            let newX, newY = (x + factor * dx, y + factor * dy)
-            pathBuilder.Append($" L%.4f{newX},%.4f{newY}") |> ignore
+            let factor = sqrt (afloat * afloat / (dir.Dx * dir.Dx + dir.Dy * dir.Dy))
 
-            calculatePath
-                (newX, newY)
-                (dx, dy)
-                pathBuilder
-                (min xMin newX)
-                (max xMax newX)
-                (min yMin newY)
-                (max yMax newY)
-                xs
+            let newPoint = {
+                X = point.X + factor * dir.Dx
+                Y = point.Y + factor * dir.Dy
+            }
+
+            pathBuilder.Append($" L%.4f{newPoint.X},%.4f{newPoint.Y}") |> ignore
+            calculatePath newPoint dir pathBuilder (updateViewBox newPoint viewBox) xs
         | Right a ->
-            let phi = atan2 dy dx
+            let phi = atan2 dir.Dy dir.Dx
             let apol = float a / 360.0 * 2.0 * Math.PI
             let newPhi = phi + apol
-            calculatePath (x, y) (cos newPhi, sin newPhi) pathBuilder xMin xMax yMin yMax xs
+            calculatePath point { Dx = cos newPhi; Dy = sin newPhi } pathBuilder viewBox xs
         | Left a ->
-            let phi = atan2 dy dx
+            let phi = atan2 dir.Dy dir.Dx
             let apol = float a / 360.0 * 2.0 * Math.PI
             let newPhi = phi - apol
-            calculatePath (x, y) (cos newPhi, sin newPhi) pathBuilder xMin xMax yMin yMax xs
+            calculatePath point { Dx = cos newPhi; Dy = sin newPhi } pathBuilder viewBox xs
         | Back a ->
             let afloat = float a
-            let factor = sqrt (afloat * afloat / (dx * dx + dy * dy))
-            let newX, newY = (x - factor * dx, y - factor * dy)
-            pathBuilder.Append($" M%.4f{newX},%.4f{newY}") |> ignore
+            let factor = sqrt (afloat * afloat / (dir.Dx * dir.Dx + dir.Dy * dir.Dy))
 
-            calculatePath
-                (newX, newY)
-                (dx, dy)
-                pathBuilder
-                (min xMin newX)
-                (max xMax newX)
-                (min yMin newY)
-                (max yMax newY)
-                xs
+            let newPoint = {
+                X = point.X - factor * dir.Dx
+                Y = point.Y - factor * dir.Dy
+            }
+
+            pathBuilder.Append($" M%.4f{newPoint.X},%.4f{newPoint.Y}") |> ignore
+            calculatePath newPoint dir pathBuilder (updateViewBox newPoint viewBox) xs
 
 let turtleToSvgPath turtlePath =
     let stringBuilder = new StringBuilder()
 
     let pathBuilder, viewBox =
-        calculatePath (0.0, 0.0) (0.0, -1.0) (stringBuilder.Append("M0,0")) 0.0 0.0 0.0 0.0 turtlePath
+        calculatePath
+            { X = 0.0; Y = 0.0 }
+            { Dx = 0.0; Dy = -1.0 }
+            (stringBuilder.Append("M0,0"))
+            {
+                XRange = { Min = 0.0; Max = 0.0 }
+                YRange = { Min = 0.0; Max = 0.0 }
+            }
+            turtlePath
 
     pathBuilder.ToString(), viewBox
 
 
 let htmlPage turtlePath =
-    let path, (xMin, xMax, yMin, yMax) = turtleToSvgPath turtlePath
+    let path, viewBox = turtleToSvgPath turtlePath
 
     html [] [
         head [] [ title [] [ str "Turtle Geometry" ] ]
         body [ attr "style" $"color: {color}" ] [
             div [ attr "align" "center" ] [
                 h1 [] [ str "Turtle Geometry" ]
-                div [] [ rawText (svg 450 450 path (xMin, xMax, yMin, yMax)) ]
+                div [] [ rawText (svg 450 450 path viewBox) ]
                 div [] [
-                    div [] [ str $"x range: [%.2f{xMin}, %.2f{xMax}]" ]
-                    div [] [ str $"y range: [%.2f{yMin}, %.2f{yMax}]" ]
+                    div [] [ str $"x range: [%.2f{viewBox.XRange.Min}, %.2f{viewBox.XRange.Max}]" ]
+                    div [] [ str $"y range: [%.2f{viewBox.YRange.Min}, %.2f{viewBox.YRange.Max}]" ]
                 ]
             ]
         ]
